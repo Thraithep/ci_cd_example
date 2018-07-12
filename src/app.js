aa1df1fcf59009928acc1706a9e0af6eb4a62fa5
thraithep <thraithep.arincare@gmail.com>
2018-07-12 11:55:52 +0700

    [~]update

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <div>
+            <p>Hello world</p> Production!!
+        </div>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
 import {render} from 'react-dom';
+import App from './app';
 
-const App = () => {
-    return(
-        <div>
-            Hello world Production!!
-        </div>
-    )
-};
-console.log(1);
 render(<App/>, document.getElementById('root'));
